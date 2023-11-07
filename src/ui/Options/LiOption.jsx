eae62d2ac2048384fc15c30f34db9ab3f811dd24
geravvene <romanov17903@gmail.com>
2023-11-07 14:21:53 +0400

    fix

--- src/ui/Options/LiOption.jsx
+++ src/ui/Options/LiOption.jsx
@@ -3,8 +3,8 @@ const LiOption = ({ children, focus, func, arg }) => {
     <li
       onClick={func}
       {...arg}
-      className={`cursor-pointer duration-200 flex gap-1 items-center pr-3 border-r-gray-dark hover:border-r-gray-light ${
-        focus ? `border-l-2 pl-3 border-l-gray-light border-r-0` : `border-r-2`
+      className={`cursor-pointer duration-200 flex gap-1 items-center pr-3 border-r-gray-dark hover:border-r-gray-light pl-3 ${
+        focus ? `border-l-2 border-l-gray-light border-r-0` : `border-r-2`
       }`}
     >
       {children}
