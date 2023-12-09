2a6f96d61bbb2cc2a6c8df7ae37f454886400861
geravvene <romanov17903@gmail.com>
2023-12-09 19:49:21 +0400

    adaptive

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -10,47 +10,45 @@ const Profile = () => {
   const user = useSelector((state) => state.userReducer);
 
   const { change, clearFavorite } = useActions();
-  
+
   return !user ? (
     <Navigate to="/reg" />
   ) : (
     <>
       <Title text={user.name}></Title>
       <div className={`content`}>
-        <div className={`flex justify-between w-fit gap-3`}>
+        <div className="flex gap-3">
           <img
             alt={`acc_icon`}
             className={`rounded-xl w-[5.5rem] h-[5.5rem]`}
             src={user.img}
           />
-          <div className={`flexcol justify-between pb-[0.1rem] `}>
+          <div className={`grid12-3 w-[100%] sm:w-[30%]`}>
             <ClassicBtn
               arg={{ onClick: () => change(null) }}
               rounded={`rounded-md`}
               color={`bg-red`}
-              className={`w-full`}
+              className={`col-span-6`}
             >
               Выход
+            </ClassicBtn>
+            
+
+            <ClassicBtn
+              rounded={`rounded-md`}
+              color={`bg-green`}
+              className={`col-span-6`}
+            >
+              <Link to={`/create`}>Создать</Link>
             </ClassicBtn>
             <ClassicBtn
               arg={{ onClick: () => clearFavorite() }}
               rounded={`rounded-lg`}
               color={`bg-blue`}
+              className={`col-span-12`}
             >
               Очистить избранное
             </ClassicBtn>
-          </div>
-
-          <div className={`flexcol justify-between pb-[0.1rem] `}>
-            <Link to={`/create`}>
-              <ClassicBtn
-                rounded={`rounded-md`}
-                color={`bg-green`}
-                className={`w-full`}
-              >
-                Создать
-              </ClassicBtn>
-            </Link>
           </div>
         </div>
         <QuotesList title={`Избранные цитаты`} />
